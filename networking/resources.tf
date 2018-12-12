7effdc0c5aa95f01e529ae0a0e88754404bda140
Garrett O'Gorman <garrett.ogorman@qstream.com>
2018-12-12 12:59:26 +0000

    Cleaned up scripts and removed NAT Gateway

--- networking/resources.tf
+++ networking/resources.tf
@@ -3,8 +3,6 @@
 ##################################################################################
 
 provider "aws" {
-  # access_key = "${var.aws_access_key}"
-  # secret_key = "${var.aws_secret_key}"
   region     = "eu-west-1"
 }
 
@@ -18,25 +16,6 @@ data "aws_availability_zones" "available" {}
 # RESOURCES
 ##################################################################################
 
-# NETWORKING #
-# module "vpc" {
-#   source = "terraform-aws-modules/vpc/aws"
-#   name   = "ddt-${terraform.workspace}"
-
-#   # cidr            = "${data.external.configuration.result.vpc_cidr_range}"
-#   cidr            = "${var.vpc_cidr_range}"
-#   azs             = "${slice(data.aws_availability_zones.available.names,0,data.external.configuration.result.vpc_subnet_count)}"
-#   private_subnets = "${data.template_file.private_cidrsubnet.*.rendered}"
-#   public_subnets  = "${data.template_file.public_cidrsubnet.*.rendered}"
-
-#   enable_nat_gateway = true
-
-#   create_database_subnet_group = false
-
-#   # tags = "${local.common_tags}"
-# }
-
-
 module "vpc" {
   source = "terraform-aws-modules/vpc/aws"
 
@@ -46,11 +25,9 @@ module "vpc" {
   private_subnets = "${data.template_file.private_cidrsubnet.*.rendered}"
   public_subnets  = "${data.template_file.public_cidrsubnet.*.rendered}"
 
-  # enable_nat_gateway           = false
   create_database_subnet_group = false
 
-  enable_nat_gateway     = true
-  single_nat_gateway     = true
+  enable_nat_gateway     = false
   one_nat_gateway_per_az = false
 
   tags = {
--- networking/variables.tf
+++ networking/variables.tf
@@ -2,14 +2,9 @@
 # VARIABLES
 ##################################################################################
 
-# variable "aws_access_key" {}
-# variable "aws_secret_key" {}
-
 variable "projectcode" {
   default = "8675309"
 }
-
-# variable "url" {}
 
 variable "vpc_cidr" {
   default = "172.16.0.0/16"
