a00c1e74429e6930939fa0ff14585cc1b14f933d
Garrett O'Gorman <garrett.ogorman@qstream.com>
2018-12-12 12:32:03 +0000

    Changed VPC Config

--- networking/variables.tf
+++ networking/variables.tf
@@ -14,3 +14,11 @@ variable "url" {}
 variable "vpc_cidr" {
   default = "172.16.0.0/16"
 }
+
+variable "public_subnet_count" {
+  default = "2"
+}
+
+variable "private_subnet_count" {
+  default = "2"
+}
