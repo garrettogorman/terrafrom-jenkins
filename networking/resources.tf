63bbd6c936e959a8eb7a1d31df5a4137fd986362
Garrett O'Gorman <garrett.ogorman@qstream.com>
2018-12-12 12:42:23 +0000

    Changed VPC Config

--- networking/resources.tf
+++ networking/resources.tf
@@ -50,8 +50,8 @@ module "vpc" {
   create_database_subnet_group = false
 
   enable_nat_gateway     = false
-  single_nat_gateway     = false
-  one_nat_gateway_per_az = false
+  # single_nat_gateway     = false
+  # one_nat_gateway_per_az = false
 
   tags = {
     Environment = "${terraform.workspace}"
